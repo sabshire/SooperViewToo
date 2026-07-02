0c2b4c8e2d8033e1f545ca6dc8df6ef0cd4c981d
Stacey Abshire <sabshire@gmail.com>
2026-07-01 22:47:19 -0400

    maybe fix permissions

--- lib/permission_handler.dart
+++ lib/permission_handler.dart
@@ -4,18 +4,19 @@ import 'dart:io' show Platform;
 
 class PermissionHandler {
   static Future<bool> hasNeededPermissions() async {
-    if (!Platform.isAndroid && !Platform.isIOS) return true;
-
-    if (await Permission.manageExternalStorage.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      return true;
+    // if (Platform.isAndroid) {
+    //   if (await Permission.manageExternalStorage.request().isGranted) {
+    //     // Either the permission was already granted before or the user just granted it.
+    //     return true;
+    //   }
+    // }
+    if (Platform.isIOS) {
+      if (await Permission.storage.request().isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        return true;
+      }
+      return false;
     }
-
-    if (await Permission.storage.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      return true;
-    }
-
-    return false;
+    return true;
   }
 }
